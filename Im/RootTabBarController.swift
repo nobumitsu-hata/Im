891b5a6347110e837594de62dcdfd04186968579
nobumitsu <nobumitsu95@gmail.com>
2019-03-12 15:12:20 +0900

    背色景透明

---
+++ Im/RootTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  RootTabBarController.swift
+//  Im
+//
+//  Created by nobumitsu on 2019/03/12.
+//  Copyright © 2019 im. All rights reserved.
+//
+
+import UIKit
+
+class RootTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
