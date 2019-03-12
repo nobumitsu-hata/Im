46a507c785489ddbd50f64b368535f37a2e2bd90
nobumitsu <nobumitsu95@gmail.com>
2019-03-12 16:04:57 +0900

    セルを用意

---
+++ Im/CommunityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommunityTableViewCell.swift
+//  Im
+//
+//  Created by nobumitsu on 2019/03/12.
+//  Copyright © 2019 im. All rights reserved.
+//
+
+import UIKit
+
+class CommunityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
