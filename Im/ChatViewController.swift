66f5bd637fb23ba3946d344db2e7cf7ec5aa1d76
nobumitsu <nobumitsu95@gmail.com>
2019-03-19 18:46:53 +0900

    チャット画面のセル作成と登録

---
+++ Im/ChatViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ChatViewController.swift
+//  Im
+//
+//  Created by nobumitsu on 2019/03/18.
+//  Copyright © 2019 im. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // 自作セルをテーブルビューに登録する
+        let chatXib = UINib(nibName: "ChatTableVIewCell", bundle: nil)
+        tableView.register(chatXib, forCellReuseIdentifier: "chatCell")
+    }
+    
+    func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "chatCell", for: indexPath) as! ChatTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tabBarController?.tabBar.isHidden = true
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
