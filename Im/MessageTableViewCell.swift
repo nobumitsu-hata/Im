c319cf459785f4e25bcacdf8ca69184501bed454
nobumitsu <nobumitsu95@gmail.com>
2019-04-30 05:56:05 +0900

    メッセージ一覧構築

---
+++ Im/MessageTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageTableViewCell.swift
+//  Im
+//
+//  Created by nobumitsu on 2019/04/29.
+//  Copyright © 2019 im. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var lastMsg: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Im/MessageViewController.swift
+++ Im/MessageViewController.swift
@@ -7,24 +7,83 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseUI
 
-class MessageViewController: UIViewController {
-
+class MessageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var ref: DatabaseReference!
+    var storage: StorageReference!
+    var dmKeyArr:[String] = []
+    var dmValArr:[String] = []
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.delegate = self
+        tableView.dataSource = self
+        // 自作セルをテーブルビューに登録する
+        let chatXib = UINib(nibName: "MessageTableViewCell", bundle: nil)
+        tableView.register(chatXib, forCellReuseIdentifier: "messageCell")
+        setupFirebase()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // ナビゲーションを透明にする処理
+        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController!.navigationBar.shadowImage = UIImage()
+        // ナビゲーションバーのテキストを変更する
+        navigationController?.navigationBar.titleTextAttributes = [
+            // 文字の色
+            .foregroundColor: UIColor.white
+        ]
+    }
+    
+    func setupFirebase() {
+        storage = Storage.storage().reference()
+        ref = Database.database().reference()
+        
+        ref.child("dmMembers").child(RootTabBarController.userId).observeSingleEvent(of: .value, with: { (snapshot) in
+            let val = snapshot.value as! [String:String]
+            self.dmKeyArr = Array(val.keys)
+            self.dmValArr = Array(val.values)
+            self.tableView.reloadData()
+        }) { (error) in
+            
+        }
+    }
+    
+    func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セル生成
+        let cell = table.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as! MessageTableViewCell
+        ref.child("users").child(dmKeyArr[indexPath.row]).observeSingleEvent(of: .value, with: { (snapshot) in
+            let val = snapshot.value as! [String:String]
+            cell.name.text = val["name"]
+            let getImg = self.storage.child("users").child(val["img"]!)
+            cell.imgView.sd_setImage(with: getImg)
+            // 角丸にする
+            cell.imgView.layer.cornerRadius = cell.imgView.frame.size.width * 0.5
+            cell.imgView.clipsToBounds = true
+            self.ref.child("directMessages").child(self.dmValArr[indexPath.row]).queryLimited(toLast: 1).observeSingleEvent(of: .value, with: { (snapshot) in
+                let snap = snapshot.value as! [String:Any]
+                let dt = Array(snap.values)
+                let dic = dt[0] as! [String:String]
+                cell.lastMsg.text = dic["message"]
+            }) { (error) in
+                
+            }
+        }) { (error) in
+            
+        }
+        
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dmKeyArr.count
     }
-    */
 
 }
